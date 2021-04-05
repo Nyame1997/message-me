7a0b8acdc60e66743e66cf861419a58120d48d0d
Ubuntu <ubuntu@ip-172-31-24-107.us-east-2.compute.internal>
2021-04-05 18:41:43 +0000

    Add user and message models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Ciara", password: "password")
+User.create(username: "Kyle", password: "password")
+User.create(username: "Jason", password: "password")
+User.create(username: "Donna", password: "password")
+User.create(username: "Conner", password: "password")
+
+Message.create(body: "Hi, this is the first message!", user_id: 1)
+Message.create(body: "The second message is in the house!", user_id: 2)
+Message.create(body: "Third message loves this new chat app!", user_id: 3)
+Message.create(body: "Number Four thinks you're all being ridiculous", user_id: 4)
+Message.create(body: "Why are we all addressing ourselves as messages?", user_id: 5)
