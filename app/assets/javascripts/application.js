bef23eccddcff99997c1a1ab2194ab8dbbb56fd6
Ubuntu <ubuntu@ip-172-31-24-107.us-east-2.compute.internal>
2021-04-06 14:51:22 +0000

    Clean layout and functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,16 @@
 scroll_bottom = function() {
   if ($('#messages').length > 0) {
     $('#messages').scrollTop($('#messages')[0].scrollHeight);
-  }
+  };
+}
+
+submit_message = function(){
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = "";
+    };
+  });
 }
 
 $(document).on('turbolinks:load', function() {
@@ -29,5 +38,6 @@ $(document).on('turbolinks:load', function() {
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  submit_message();
   scroll_bottom();
 })
